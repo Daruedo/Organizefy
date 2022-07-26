a15441110ce7c60e5d0b30a13d6f4bff551f8afe
Lucas Eduardo <daruedo@gmail.com>
2022-07-26 16:01:40 -0300

    adicionando ciclo de pagamentos

--- angular/README.md
+++ angular/README.md
@@ -3,3 +3,10 @@
 Iniciar:
 
 npm run dev
+
+Instalar?
+
+npm install pm2 -g
+
+ng-if="bcCtrl.tabList" ng-class="{active: bcCtrl.tabList}"
+ng-controller="BillingCycleCtrl as bcCtrl"
--- node/config/routes.js
+++ node/config/routes.js
@@ -4,9 +4,10 @@ module.exports = function(server) {
     const router = express.Router();
     server.use('/api', router);
 
-    const billingCycleService = require('./api/billingCycle/billingCycleService');
+    const billingCycleService = require('../api/billingCycle/billingCycleService');
     billingCycleService.register(router, '/billingCycles');
 
-    const billingSummaryService = require('./api/billingSummary/billingSummaryService')
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService')
     router.route('/billingSummary').get(billingSummaryService.getSummary)
 }
